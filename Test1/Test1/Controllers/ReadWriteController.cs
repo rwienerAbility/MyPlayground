0c03768d89b01fcfae6e97b24c460dc6a4a29c87
Robert Wiener <robert.wiener@abilitynetwork.com>
2014-10-29 15:36:04 -0400

    Added new API Controller

---
+++ Test1/Test1/Controllers/ReadWriteController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Test1.Controllers
+{
+    public class ReadWriteController : ApiController
+    {
+        // GET: api/ReadWrite
+        public IEnumerable<int> Get()
+        {
+            return new int[] { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+        }
+
+        // GET: api/ReadWrite/5
+        public int Get(int id)
+        {
+            return id * 10;
+        }
+
+        // POST: api/ReadWrite
+        public void Post([FromBody]int value)
+        {
+        }
+
+        // PUT: api/ReadWrite/5
+        public void Put(int id, [FromBody]int value)
+        {
+        }
+
+        // DELETE: api/ReadWrite/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
